bcb9b0d36d359f961d68e783c6908851005c209d
Andrea Campi <andrea.campi@zephirworks.com>
2012-08-17 10:57:47 +0200

    Bump cookbook versions after refactoring.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cashion@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry-dea"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.99.0"
 
 %w{ ubuntu }.each do |os|
   supports os
